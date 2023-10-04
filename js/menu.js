c2dc1d7b81a6f7d8f5b02f94167b40f3d694e512
abaranano <118171707+abaranano@users.noreply.github.com>
2023-10-04 14:10:48 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -1,12 +1,12 @@
 multilineString =
   '<div>'+
   '<div class="d-flex flex-row nowrap">'+
-  '<img style="height: 50px;" class="" src="img/logo_mallorca.png">'+
+  '<img style="height: 50px;" class="" src="images/logo_mallorca.png">'+
   '<h1>Mallorca</h1>'+
   '</div>'+
   '<ul class="nav justify-content-center d-flex flex-column flex-lg-row flex-md-row align-content-center bg-dark">'+
   '<li class="nav-item">'+
-  '<a class="nav-link text-light" href="#">Menu 1</a>'+
+  '<a class="nav-link text-light" href="html/pagina1.html">pagina 1</a>'+
   '</li>'+
   '<li class="nav-item">'+
   '<a class="nav-link text-light" href="#">Menu 2</a>'+
@@ -15,7 +15,7 @@ multilineString =
   '<a class="nav-link text-light" href="#">Menu 3</a>'+
   '</li>'+
   '<li class="nav-item">'+
-  '<a class="nav-link text-light" href="#">Menu 4</a>'+
+  '<a class="nav-link text-light" href="html/formulario.html">formulario</a>'+
   '</li>'+
   '</ul>'+
   '</div>';
