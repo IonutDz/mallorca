1e1bb4f23464de64b466f9947de6a25d0deae2e0
Arkaitz5 <a_ortiz@fpzornotza.com>
2023-10-04 14:50:40 +0200

    Update formulario.js

--- js/formulario.js
+++ js/formulario.js
@@ -16,38 +16,37 @@ function validarFormulario() {
         camposValidos = false;
     }
 
-    if(seleccionarActividad.value.trim() === ""){
+    if (seleccionarActividad.value.trim() === "") {
         alert('Debe seleccionar una actividad');
         camposValidos = false;
     }
 
-    if(seleccionarNombre.value.trim() === ""){
+    if (seleccionarNombre.value.trim() === "") {
         alert('Debe ingresar su nombre');
         camposValidos = false;
     }
 
-    if(seleccionEmail.value.trim() === ""){
+    if (seleccionEmail.value.trim() === "") {
         alert('Debe ingresar un correo electronico')
         camposValidos = false;
     }
 
-    if(seleccionEmailRepe.value.trim() === ""){
+    if (seleccionEmailRepe.value.trim() === "") {
         alert('Debe ingresar el mismo correo electronico')
         camposValidos = false;
     }
 
-    function validarCorreoElectronico() {
-        // Verificar si el correo tiene un formato válido (puedes usar una expresión regular)
-        const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
-        return re.test(emailinput.value);
+    if (!validarCorreoElectronico()) {
+        alert("El campo de correo electrónicono no es valido");
+        camposValidos = false;
     }
 
-    if(seleccionAdjuntar.value.trim() === ""){
+    if (seleccionAdjuntar.value.trim() === "") {
         alert('Debe adjuntar algo')
         camposValidos = false;
     }
 
-    if(!seleccionCheckbox.checked){
+    if (!seleccionCheckbox.checked) {
         alert('Debe aceptar los términos y la política de privacidad');
         camposValidos = false;
     }
@@ -58,11 +57,28 @@ function validarFormulario() {
     return camposValidos;
 }
 
-document.getElementById("fincontacto").addEventListener("submit" , function(event){
-    if  (!validarFormulario()) {
+function validarCorreoElectronico() {
+    // Verificar si el correo tiene un formato válido (puedes usar una expresión regular)
+    const re = /^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/;
+    return re.test(seleccionEmail.value);
+}
+document.getElementById("fincontacto").addEventListener("submit", function (event) {
+    if (!validarFormulario()) {
         event.preventDefault();
-    } else{
-        alert("Todo Correcto")
+    } else {
+        alert("¡Todo correcto!");
+
+        // Reiniciar el formulario para borrar los datos ingresados
+        document.getElementById("fincontacto").reset();
     }
 });
 
+seleccionEmail = document.getElementById("selectcorreo");
+seleccionEmailRepe = document.getElementById("selectrepecorreo");
+seleccionEmail.addEventListener("input", function () {
+    if (seleccionEmail.value.trim() !== '') {
+        seleccionEmailRepe.disabled = false; // Habilitar el campo de apellido
+    } else {
+        seleccionEmailRepe.disabled = true; // Desactivar el campo de apellido
+    }
+});
