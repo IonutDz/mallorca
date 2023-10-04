948150ca0b6433b4227d46a1003af9cb4328a81e
Arkaitz5 <a_ortiz@fpzornotza.com>
2023-10-04 14:27:00 +0200

    s

--- js/formulario.js
+++ js/formulario.js
@@ -1,18 +1,53 @@
 selecionarAsunto = document.getElementById("selectasunto");
-actividadSeleccionar = document.getElementById("exampleInputPassword1")
+seleccionarActividad = document.getElementById("selectactividad");
+seleccionarNombre = document.getElementById("selectnombre");
+seleccionEmail = document.getElementById("selectcorreo");
+seleccionEmailRepe = document.getElementById("selectrepecorreo");
+seleccionCampoAlto = document.getElementById("selectcampoalto");
+seleccionAdjuntar = document.getElementById("adjuntarArchivo");
+seleccionCheckbox = document.getElementById("checkbox");
+
 
 function validarFormulario() {
     let camposValidos = true;
 
     if (selecionarAsunto.value.trim() === "") {
-        alert("El asunto no puede estar vacio");
+        alert('El asunto no puede estar vacio');
         camposValidos = false;
     }
 
-    if(actividadSeleccionar.value.trim() === ""){
-        alert("Debe seleccionar una actividad");
+    if(seleccionarActividad.value.trim() === ""){
+        alert('Debe seleccionar una actividad');
         camposValidos = false;
     }
+
+    if(seleccionarNombre.value.trim() === ""){
+        alert('Debe ingresar su nombre');
+        camposValidos = false;
+    }
+
+    if(seleccionEmail.value.trim() === ""){
+        alert('Debe ingresar un correo electronico')
+        camposValidos = false;
+    }
+
+    if(seleccionEmailRepe.value.trim() === ""){
+        alert('Debe ingresar el mismo correo electronico')
+        camposValidos = false;
+    }
+
+    if(seleccionAdjuntar.value.trim() === ""){
+        alert('Debe adjuntar algo')
+        camposValidos = false;
+    }
+
+    if(!seleccionCheckbox.checked){
+        alert('Debe aceptar los términos y la política de privacidad');
+        camposValidos = false;
+    }
+
+
+
 
     return camposValidos;
 }
