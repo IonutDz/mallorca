c68953db4bb8355fc40bb3e7729db5540e2e0dfb
abaranano <118171707+abaranano@users.noreply.github.com>
2023-10-09 10:39:01 +0200

    navbar y footer

--- js/menu.js
+++ js/menu.js
@@ -6,7 +6,7 @@ multilineString =
   '</a>'+
   '</div>'+
   '<nav class="d-flex flex-row bg-dark">'+
-  '<ul class="nav justify-content-center d-flex flex-column flex-lg-row flex-md-row align-content-center col-11">'+
+  '<ul class="nav justify-content-center d-flex flex-column flex-lg-row flex-md-row align-content-center col-12">'+
   '<li class="nav-item">'+
   '<a class="nav-link text-light" href="/html/comida.html">Comida</a>'+
   '</li>'+
