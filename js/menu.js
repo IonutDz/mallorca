78d5bcb60c5397130e4064ad12cbe3ac0560d1b5
IonutDz <i_dimasca@fpzornotza.com>
2023-10-09 08:51:35 +0200

    Update menu.js

--- js/menu.js
+++ js/menu.js
@@ -6,7 +6,7 @@ multilineString =
   '<nav class="d-flex flex-row bg-dark">'+
   '<ul class="nav justify-content-center d-flex flex-column flex-lg-row flex-md-row align-content-center col-11">'+
   '<li class="nav-item">'+
-  '<a class="nav-link text-light" href="html/pagina1.html">pagina 1</a>'+
+  '<a class="nav-link text-light" href="/html/pagina1.html">pagina 1</a>'+
   '</li>'+
   '<li class="nav-item">'+
   '<a class="nav-link text-light" href="#">Menu 2</a>'+
@@ -15,7 +15,7 @@ multilineString =
   '<a class="nav-link text-light" href="#">Menu 3</a>'+
   '</li>'+
   '<li class="nav-item">'+
-  '<a class="nav-link text-light" href="html/formulario.html">formulario</a>'+
+  '<a class="nav-link text-light" href="/html/formulario.html">formulario</a>'+
   '</li>'+
   '</ul>'+
   '<div class="col-1 d-flex justify-content-center">'+
