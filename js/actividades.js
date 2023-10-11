250758f0ac7181370376f216f12b4f1e196c0898
IonutDz <i_dimasca@fpzornotza.com>
2023-10-11 10:31:44 +0200

    a

--- js/actividades.js
+++ js/actividades.js
@@ -3,153 +3,82 @@ var texto;
 
 var inputs = document.querySelectorAll('input');
 
-inputs.forEach(function(input) {
+inputs.forEach(function (input) {
     input.addEventListener("click", function() {
-
-        switch(this.value){
-            case "cuevas de hams":
-               ruta='/images/cuevas del hams.jpg';
-               texto='Cuevas de hans';
-            break;
-                
-            case "delfines":
-                ruta='/images/delfines.jpg';
-                texto='Avistamiento de delfines';
-            break;
-    
-            case "crucero en catamarán":
-                ruta='/images/catamarán.jpg';
-                texto='Crucero en catamarán';
-            break;
-    
-            case "santa maría":
-                ruta='/images/santa_maria.jpg';
-                texto='Catedrall de santa María';
-            break;
-    
-            case "bellver":
-                ruta='/images/bellver.jpg';
-                texto='Castillo de Bellver';
-            break;
-    
-            case "Sóller":
-                ruta='/images/Sóller.jpg';
-                texto='Ferrocarril de Sóller';
-            break;
-    
-            case "valldemosa":
-                ruta='/images/valldemosa.jpg';
-                texto='Valldemosa';
-            break;
-    
-            case "tramontana":
-                ruta='/images/Tramontana.jpg';
-                texto='Sierra de Tramontana';
-            break;
-    
-        }
-
-        multilineString= '<!DOCTYPE html>'+
-        '<html>'+
-        '<head>'+
-        '<meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″ />'+
-        '<meta name="viewport" content="width=device-width, initial-scale=1.0">'+
-        '<title>Mallorca</title>'+
-        '<link rel="shortcut icon" href="/images/logo_mallorca.png" type="image/x-icon" />'+
-        '<link href="/css/bootstrap.min.css" rel="stylesheet">'+
-        '<link href="/css/style.css" rel="stylesheet">'+
-        '</head>'+
-        '<body>'+
-        '<!-- navbar-->'+
-        '<script src="/js/menu.js"></script>'+
-        '<!-- navbar-->'+
-        '<img src="'+ruta+'" style="height: 200px;">'+
-        '<h1>'+texto+'</h1>'+
-        '<!--footer-->'+
-        '<script src="/js/footer.js"></script>'+
-        '<!--footer-->'+
-        '<script src="/js/actividades.js" type="text/javascript"></script>'+
-        '<script src="/js/bootstrap.bundle.min.js"></script>'+
-        '</body>'+
-        '</html>';
-
-    var ventana=window.open();
-    ventana.document.write(multilineString);
+        crearActividad(input.value);
     });
 });
-/*
-document.querySelectorAll('input').addEventListener ("click", function() {
-    
-    console.log("Messi calvo");
 
-    switch(this.value){
+function crearActividad(valor) {
+   
+    switch (valor) {
         case "cuevas de hams":
-           ruta='/images/cuevas del hams.jpg';
-           texto='Cuevas de hans';
-        break;
-            
+            ruta = '/images/cuevas del hams.jpg';
+            texto = 'Cuevas de hans';
+            break;
+
         case "delfines":
-            ruta='/images/delfines.jpg';
-            texto='Avistamiento de delfines';
-        break;
+            ruta = '/images/delfines.jpg';
+            texto = 'Avistamiento de delfines';
+            break;
 
         case "crucero en catamarán":
-            ruta='/images/catamarán.jpg';
-            texto='Crucero en catamarán';
-        break;
+            ruta = '/images/catamarán.jpg';
+            texto = 'Crucero en catamarán';
+            break;
 
         case "santa maría":
-            ruta='/images/santa_maria.jpg';
-            texto='Catedrall de santa María';
-        break;
+            ruta = '/images/santa_maria.jpg';
+            texto = 'Catedrall de santa María';
+            break;
 
         case "bellver":
-            ruta='/images/bellver.jpg';
-            texto='Castillo de Bellver';
-        break;
+            ruta = '/images/bellver.jpg';
+            texto = 'Castillo de Bellver';
+            break;
 
         case "Sóller":
-            ruta='/images/Sóller.jpg';
-            texto='Ferrocarril de Sóller';
-        break;
+            ruta = '/images/Sóller.jpg';
+            texto = 'Ferrocarril de Sóller';
+            break;
 
         case "valldemosa":
-            ruta='/images/valldemosa.jpg';
-            texto='Valldemosa';
-        break;
+            ruta = '/images/valldemosa.jpg';
+            texto = 'Valldemosa';
+            break;
 
         case "tramontana":
-            ruta='/images/Tramontana.jpg';
-            texto='Sierra de Tramontana';
-        break;
+            ruta = '/images/Tramontana.jpg';
+            texto = 'Sierra de Tramontana';
+            break;
 
     }
 
-    multilineString= '<!DOCTYPE html>'+
-    '<html>'+
-    '<head>'+
-        '<meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″ />'+
-        '<meta name="viewport" content="width=device-width, initial-scale=1.0">'+
-        '<title>Mallorca</title>'+
-        '<link rel="shortcut icon" href="/images/logo_mallorca.png" type="image/x-icon" />'+
-        '<link href="/css/bootstrap.min.css" rel="stylesheet">'+
-        '<link href="/css/style.css" rel="stylesheet">'+
-    '</head>'+
-    '<body>'+
-        '<!-- navbar-->'+
-        '<script src="/js/menu.js"></script>'+
-        '<!-- navbar-->'+
-        '<img src="'+ruta+'" style="height: 200px;">'+
-        '<h1>'+texto+'</h1>'+
-        '<!--footer-->'+
-        '<script src="/js/footer.js"></script>'+
-        '<!--footer-->'+
-        '<script src="/js/actividades.js" type="text/javascript"></script>'+
-        '<script src="/js/bootstrap.bundle.min.js"></script>'+
-    '</body>'+
-'</html>';
+    multilineString = '<!DOCTYPE html>' +
+        '<html>' +
+        '<head>' +
+        '<meta http-equiv=”Content-Type” content=”text/html; charset=UTF-8″ />' +
+        '<meta name="viewport" content="width=device-width, initial-scale=1.0">' +
+        '<title>Mallorca</title>' +
+        '<link rel="shortcut icon" href="/images/logo_mallorca.png" type="image/x-icon" />' +
+        '<link href="/css/bootstrap.min.css" rel="stylesheet">' +
+        '<link href="/css/style.css" rel="stylesheet">' +
+        '</head>' +
+        '<body>' +
+        '<!-- navbar-->' +
+        '<script src="/js/menu.js"></script>' +
+        '<!-- navbar-->' +
+        '<img src="' + ruta + '" style="height: 200px;">' +
+        '<h1>' + texto + '</h1>' +
+        '<!--footer-->' +
+        '<script src="/js/footer.js"></script>' +
+        '<!--footer-->' +
+        '<script src="/js/actividades.js" type="text/javascript"></script>' +
+        '<script src="/js/bootstrap.bundle.min.js"></script>' +
+        '</body>' +
+        '</html>';
 
-    var ventana=window.open();
+    var ventana = window.open();
     ventana.document.write(multilineString);
-   
-});*/
+}
+
--- js/menu3.js
+++ js/menu3.js
@@ -1,46 +1,19 @@
-// menu3.js
-// Primero, carga el archivo actividades.js
-var script = document.createElement('script');
-script.src = '/js/actividades.js';
-document.head.appendChild(script);
-
-// Luego, agrega el código para abrir la ventana emergente al hacer clic en el botón
-var boton = document.getElementById("mostrarActividad");
-
-boton.addEventListener("click", function () {
-    // Asegúrate de que la variable actividades se haya cargado desde actividades.js
-    if (typeof actividades !== "undefined" && actividades.length > 0) {
-        // Elegir una actividad al azar
+var actividades = [
+    "Visita a las Cuevas del Hams",
+    "Avistamiento de delfines",
+    "Crucero en Catamarán",
+    "Basílica de Santa María",
+    "Castillo de Bellver",
+    "Puerto de Sóller",
+    "La Cartuja de Valldemossa",
+    "Sierra de Tramontana"
+  ];
+  
+  document.addEventListener("DOMContentLoaded", function () {
+    var boton = document.querySelector(".btn-pulsar"); // Selecciona el botón por su clase "btn-pulsar"
+    boton.addEventListener("click", function () {
         var actividadRandom = actividades[Math.floor(Math.random() * actividades.length)];
+        crearActividad
+    });
+  });
 
-        // Crear una nueva ventana emergente
-        var ventanaEmergente = window.open('', '_blank');
-
-        // Crear el contenido HTML para la ventana emergente
-        var contenidoHTML = `
-            <!DOCTYPE html>
-            <html>
-            <head>
-                <meta charset="UTF-8">
-                <meta name="viewport" content="width=device-width, initial-scale=1.0">
-                <title>Actividad Aleatoria</title>
-                <link rel="shortcut icon" href="/images/logo_mallorca.png" type="image/x-icon">
-                <link href="/css/bootstrap.min.css" rel="stylesheet">
-                <link href="/css/style.css" rel="stylesheet">
-            </head>
-            <body>
-                <h1>${actividadRandom.nombre}</h1>
-                <img src="${actividadRandom.imagen}" style="height: 200px;">
-                <p>${actividadRandom.descripcion}</p>
-                <!-- Puedes agregar más detalles o contenido relacionado a la actividad aquí -->
-            </body>
-            </html>
-        `;
-
-        // Escribir el contenido en la ventana emergente
-        ventanaEmergente.document.write(contenidoHTML);
-    } else {
-        // Manejar el caso en el que actividades no se haya cargado correctamente
-        alert("No se pudieron cargar las actividades. Por favor, intenta nuevamente.");
-    }
-});
