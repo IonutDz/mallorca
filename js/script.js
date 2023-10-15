aac3866376a28e5dfe66876e4e513aafb584ccac
IonutDz <i_dimasca@fpzornotza.com>
2023-10-15 15:19:26 +0200

    a

--- js/script.js
+++ js/script.js
@@ -1,22 +1,24 @@
-let items = document.querySelectorAll('.carousel .carousel-item')
-//clona para al menos hacer 4 items en cada slide
-items.forEach((el) => {
-    const minPerSlide = 4
-    let next = el.nextElementSibling
-    for (var i = 1; i < minPerSlide; i++) {
+function cloneItemsInCarousel(carouselSelector) {
+  let carousel = document.querySelector(carouselSelector);
+  if (carousel) {
+    let items = carousel.querySelectorAll(".carousel .carousel-item");
+    items.forEach((el) => {
+      const minPerSlide = 4;
+      let next = el.nextElementSibling;
+      for (var i = 1; i < minPerSlide; i++) {
         if (!next) {
-            next = items[0]
+          next = items[0];
         }
-        let cloneChild = next.cloneNode(true)
-        el.appendChild(cloneChild.children[0])
-        next = next.nextElementSibling
-    }
-})
+        let cloneChild = next.cloneNode(true);
+        el.appendChild(cloneChild.children[0]);
+        next = next.nextElementSibling;
+      }
+    });
+  }
+}
 
+// Clona elementos en el primer carrusel
+cloneItemsInCarousel("#recipeCarousel");
 
-
-
-
-
-
-
+// Clona elementos en el segundo carrusel (si tienes otro)
+cloneItemsInCarousel("#recipeCarousel2");
--- js/slider.js
+++ js/slider.js
@@ -1,45 +1,25 @@
-const carousel = document.querySelector(".carousel");
-
-let isDown = false;
-
-let startX;
-
-let scrollLeft;
-
-carousel.addEventListener("mousedown", (e) => {
-
-isDown = true;
-
-startX = e.pageX - carousel.offsetLeft;
-
-scrollLeft = carousel.scrollLeft;
-
-});
-
-carousel.addEventListener("mouseleave", () => {
-
-isDown = false;
-
-});
-
-carousel.addEventListener("mouseup", () => {
-
-isDown = false;
-
-});
-
-carousel.addEventListener("mousemove", (e) => {
-
-if (!isDown) return;
-
-e.preventDefault();
-
-const x = e.pageX - carousel.offsetLeft;
-
-const walk = x - startX;
-
-carousel.scrollLeft = scrollLeft - walk;
-
-});
-
-
+function cloneItemsInCarousel(carouselSelector) {
+    let carousel = document.querySelector(carouselSelector);
+    if (carousel) {
+      let items = carousel.querySelectorAll(".carousel .carousel-item");
+      items.forEach((el) => {
+        const minPerSlide = 4;
+        let next = el.nextElementSibling;
+        for (var i = 1; i < minPerSlide; i++) {
+          if (!next) {
+            next = items[0];
+          }
+          let cloneChild = next.cloneNode(true);
+          el.appendChild(cloneChild.children[0]);
+          next = next.nextElementSibling;
+        }
+      });
+    }
+  }
+  
+  // Clona elementos en el primer carrusel
+  cloneItemsInCarousel("#recipeCarousel");
+  
+  // Clona elementos en el segundo carrusel (si tienes otro)
+  cloneItemsInCarousel("#recipeCarousel2");
+  
