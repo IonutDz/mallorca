f1f6ce9375c4a5cd2b32ad026b231a74fd86c45e
IonutDz <i_dimasca@fpzornotza.com>
2023-10-15 22:52:51 +0200

    a

--- js/slider.js
+++ js/slider.js
@@ -1,25 +1,28 @@
 function cloneItemsInCarousel(carouselSelector) {
-    let carousel = document.querySelector(carouselSelector);
-    if (carousel) {
-      let items = carousel.querySelectorAll(".carousel .carousel-item");
-      items.forEach((el) => {
-        const minPerSlide = 4;
-        let next = el.nextElementSibling;
-        for (var i = 1; i < minPerSlide; i++) {
-          if (!next) {
-            next = items[0];
-          }
-          let cloneChild = next.cloneNode(true);
-          el.appendChild(cloneChild.children[0]);
-          next = next.nextElementSibling;
+  let carousel = document.querySelector(carouselSelector);
+  if (carousel) {
+    let items = carousel.querySelectorAll(".carousel .carousel-item");
+    items.forEach((el) => {
+      const minPerSlide = 4;
+      let next = el.nextElementSibling;
+      for (var i = 1; i < minPerSlide; i++) {
+        if (!next) {
+          next = items[0];
         }
-      });
-    }
+        let cloneChild = next.cloneNode(true);
+        el.appendChild(cloneChild.children[0]);
+        next = next.nextElementSibling;
+      }
+    });
   }
-  
-  // Clona elementos en el primer carrusel
-  cloneItemsInCarousel("#recipeCarousel");
-  
-  // Clona elementos en el segundo carrusel (si tienes otro)
-  cloneItemsInCarousel("#recipeCarousel2");
-  
+}
+
+function countAndLogCarousels() {
+  const carousels = document.querySelectorAll(".carousel");
+  carousels.forEach((carousel) => {
+    const carouselId = carousel.getAttribute("id");
+    cloneItemsInCarousel(`#${carouselId}`);
+  });
+}
+
+countAndLogCarousels();
